89d6d70258f6a865a1c8faea718938a9f7d3f947
Paarth Batra <138647078+hydro-7@users.noreply.github.com>
2026-03-27 10:00:53 +0530

    Fixed Mutex Sum Code

--- PThreads/mutex_sum.cpp
+++ PThreads/mutex_sum.cpp
@@ -2,42 +2,52 @@
 #include <pthread.h>
 using namespace std;
 
-#define N 4
+#define N 100
 
-int A[N][N], B[N][N];
+int arr[N];
+int num_threads = 4;
+int total_sum = 0;
 
-void* transpose(void* arg) {
-    int row = *(int*)arg;
+pthread_mutex_t lock;
 
-    for(int j = 0; j < N; j++) {
-        B[j][row] = A[row][j];
+void* sum(void* arg) {
+    int id = *(int*)arg;
+    int chunk = N / num_threads;
+
+    int start = id * chunk;
+    int end = start + chunk;
+
+    int local_sum = 0;
+
+    for(int i = start; i < end; i++) {
+        local_sum += arr[i];
     }
+
+    pthread_mutex_lock(&lock);
+    total_sum += local_sum;
+    pthread_mutex_unlock(&lock);
 
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t threads[N];
-    int rows[N];
+    pthread_t threads[4];
+    int ids[4];
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            A[i][j] = i * N + j;
-        }
-    }
-
-    for(int i = 0; i < N; i++) {
-        rows[i] = i;
-        pthread_create(&threads[i], NULL, transpose, &rows[i]);
-    }
+    pthread_mutex_init(&lock, NULL);
 
     for(int i = 0; i < N; i++)
+        arr[i] = i + 1;
+
+    for(int i = 0; i < num_threads; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, sum, &ids[i]);
+    }
+
+    for(int i = 0; i < num_threads; i++)
         pthread_join(threads[i], NULL);
 
-    cout << "Transpose:\n";
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++)
-            cout << B[i][j] << " ";
-        cout << endl;
-    }
+    cout << "Total Sum = " << total_sum << endl;
+
+    pthread_mutex_destroy(&lock);
 }
